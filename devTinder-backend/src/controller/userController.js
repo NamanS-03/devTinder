5972483c99b38d75d5c34744a55dfa01592bea81
NamanS-03 <naman.mzp03@gmail.com>
2026-07-27 09:26:32 +0530

    API for getting the list connections of logged In user

--- devTinder-backend/src/controller/userController.js
+++ devTinder-backend/src/controller/userController.js
@@ -31,6 +31,47 @@ const pendingConnectionRequest = async (req, res) => {
     }
 }
 
+const acceptedConnectionRequest = async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, status: "accepted"},
+                { toUserId: loggedInUser._id, status: "accepted"}
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+        if(!connectionRequests) {
+            return res.status(400).json({
+                message: "No Connections Present"
+            })
+        }
+
+        // need to understand this
+        // need filtering because we only need the data of loggedInUSer's connection and there is no need to send the information about 
+        // loggedInUser to the frontend. 
+        const data = connectionRequests.map((row) => {
+            if(row.fromUserId._id.toString() == loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        })
+
+        res.status(200).json({
+            message: loggedInUser.firstName + "'s Connections ",
+            data
+        })
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
 module.exports = {
-    pendingConnectionRequest
+    pendingConnectionRequest,
+    acceptedConnectionRequest
 }
--- devTinder-backend/src/routes/userRoutes.js
+++ devTinder-backend/src/routes/userRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { peopleAuth } = require('../middleware/authMiddleware')
 const { 
-    pendingConnectionRequest
+    pendingConnectionRequest,
+    acceptedConnectionRequest
 } = require('../controller/userController')
 
 // getting the list of all the pending requests for the logged in user
 router.get('/receivedRequest', peopleAuth, pendingConnectionRequest);
+
+// getting the list of my connections 
+router.get('/myConnections', peopleAuth, acceptedConnectionRequest);
 
 module.exports = router;
